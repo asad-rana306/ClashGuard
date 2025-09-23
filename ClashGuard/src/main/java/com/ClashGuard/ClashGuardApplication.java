8d353f906e85a40924e4f888e3e147610503a1fd
Rana Asad <asadcodecraft@gmail.com>
2025-09-23 14:56:21 +0500

    Add project files

---
+++ ClashGuard/src/main/java/com/ClashGuard/ClashGuardApplication.java
@@ -0,0 +1,13 @@
+package com.ClashGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashGuardApplication.class, args);
+	}
+
+}
---
+++ ClashGuard/src/test/java/com/ClashGuard/ClashGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClashGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
